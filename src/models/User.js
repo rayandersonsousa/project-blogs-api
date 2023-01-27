854287c5c7866d2fdf04c84a6ab6408aaa5c5eff
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-01-27 16:15:52 -0300

    Requisito 02 finalizado

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const UserModel = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users',
+  });
+
+  return UserTable;
+};
+
+module.exports = UserModel;
