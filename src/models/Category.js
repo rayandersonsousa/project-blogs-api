ee3e23914a6f437de5a665253314ccfcef1261a9
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-01-30 20:23:30 -0300

    Requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'categories',
+  });
+
+  return Category;
+};
