551bf14b3373207353c6e4cf5359fd1fc539986f
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-01-31 15:40:55 -0300

    Requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'post_category',
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
